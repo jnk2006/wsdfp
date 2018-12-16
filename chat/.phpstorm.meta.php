f8d2b54cc92eb7e7a1b7b8de71c6080c149b26b2
naveena <jnk22@njit.edu>
2018-12-16 16:09:56 -0500

    Added procfile and readme file.

--- chat/.phpstorm.meta.php
+++ chat/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-12-16 19:26:45.
+    * Generated on 2018-12-16 21:08:01.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
